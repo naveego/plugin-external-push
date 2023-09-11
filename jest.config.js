c2d043b5953669f5212031e276213d89ce948af0
roehlerw <wyatt.roehler@aunalytics.com>
2023-09-11 15:19:28 -0400

    feat(build): added build commands

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// Sync object
+var config = {
+    verbose: true,
+    transform: {
+        '^.+\\.tsx?$': 'ts-jest',
+    },
+};
+exports.default = config;
